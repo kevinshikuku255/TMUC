9b3c152930d4f8df2ce82ffad8b68c2004d2da10
kevin <kevinshikuku255@gmail.com>
2021-07-16 17:22:02 +0300

    grid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import Index from "./Pages"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Tom Mboya University
-        </p>
-        <a
-          className="App-link"
-          href="./Images/Tom.jpeg"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          TMUC
-        </a>
+        <p>Tom Mboya University</p>
       </header>
+      <Index/>
     </div>
   );
 }
---
+++ src/Pages/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import "./pages.css"
+
+function Index() {
+  return (
+  <div className="flex">
+      <div class="grid-container">
+          <div class="grid-item">
+              <a href="http://student.tmuc.ac.ke/" >Student Login</a></div>
+          <div class="grid-item">
+             <a href="https://elearning.tmuc.ac.ke/">E-learning</a>
+          </div>
+          <div class="grid-item">3</div>
+          <div class="grid-item">4</div>
+          <div class="grid-item">5</div>
+          <div class="grid-item">6</div>
+          <div class="grid-item">7</div>
+          <div class="grid-item">8</div>
+          <div class="grid-item">9</div>
+          <div class="grid-item">4</div>
+          <div class="grid-item">5</div>
+          <div class="grid-item">6</div>
+          <div class="grid-item">7</div>
+          <div class="grid-item">8</div>
+          <div class="grid-item">9</div>
+          <div class="grid-item">4</div>
+          <div class="grid-item">5</div>
+          <div class="grid-item">6</div>
+          <div class="grid-item">7</div>
+          <div class="grid-item">8</div>
+                    <div class="grid-item">7</div>
+          <div class="grid-item">8</div>
+          <div class="grid-item">9</div>
+          <div class="grid-item">4</div>
+          <div class="grid-item">5</div>
+          <div class="grid-item">6</div>
+          <div class="grid-item">7</div>
+          <div class="grid-item">8</div>
+          <div class="grid-item">9</div>
+      </div>
+  </div>
+  )
+}
+
+export default Index;
