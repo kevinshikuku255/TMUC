2583a15badc1dde614f1f9413dd3ce6b3f4904aa
Kevin Shikuku <68957078+kevinshikuku255@users.noreply.github.com>
2021-07-17 12:26:53 +0300

    Update StudentCouncil.js

--- src/Pages/StudentCouncil.js
+++ src/Pages/StudentCouncil.js
@@ -4,7 +4,7 @@ import "./pages.css"
 function StudentCouncil() {
   return (
     <div className="StudentCouncil">
-       <h4>Student Council</h4>
+       <h4>Student - Council</h4>
        <button className="P"> SOTMOC</button>
     </div>
   )
