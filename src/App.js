0f42aed525cf91e8d5e52ba673597ef55a4f7dd7
kevin <kevinshikuku255@gmail.com>
2021-07-17 08:29:08 +0300

    google analytics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import ReactGA from 'react-ga';
 import Index from "./Pages";
+import { REACT_GA } from "./config.json"
 
 import Logo from "./Images/tom_logo2.png"
 import './App.css';
 
+
+ReactGA.initialize(REACT_GA);
+ReactGA.pageview(window.location.pathname + window.location.search);
 function App() {
   return (
     < div className="App">
