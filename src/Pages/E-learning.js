6f56d4de0d5b9473c02afab291a025db3b62b7d9
kevin <kevinshikuku255@gmail.com>
2021-07-21 12:40:56 +0300

    e-laerning

--- src/Pages/E-learning.js
+++ src/Pages/E-learning.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import {Link} from"react-router-dom";
+// import {Link} from"react-router-dom";
 import "./pages.css";
-import { useLoadDispatch } from '../Context/loading';
+// import { useLoadDispatch } from '../Context/loading';
 
 function Elearning() {
 
-   const loadDispatch = useLoadDispatch();
+  //  const loadDispatch = useLoadDispatch();
 
 
-   const clickHandler = () => {
-     loadDispatch({
-       type: "LOAD",
-       payload: true
-     })
-   }
+  //  const clickHandler = () => {
+  //    loadDispatch({
+  //      type: "LOAD",
+  //      payload: true
+  //    })
+  //  }
 
   return (
     <div className="Elearning">
        <h4>E-learning</h4>
-       <button className="E" onClick={clickHandler}>
-          <Link to="/e-learning">E-learning</Link>
+       <button className="E" >
+          {/* <Link to="/e-learning">E-learning</Link> */}
           {/* <a href="https://elearning.tmuc.ac.ke/">E-learning</a> */}
+          E-learning
         </button>
     </div>
   )
