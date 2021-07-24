53a9f6f862c1ca0d1c3ca572a79619db059ccbdf
kevin <kevinshikuku255@gmail.com>
2021-07-24 12:25:50 +0300

    photos

---
+++ src/Pages/Photos.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./pages.css";
+import { useLoadDispatch } from '../Context/loading';
+
+
+/** Photos from google maps */
+function Photos() {
+
+   const loadDispatch = useLoadDispatch();
+
+
+   const clickHandler = () => {
+     loadDispatch({
+       type: "LOAD",
+       payload: true
+     })
+   }
+
+  return (
+    <div className="Photos">
+       <h4>Photos</h4>
+       <button className="P" onClick={clickHandler}>
+         <a href="https://www.google.com/maps/uv?pb=!1s0x19d4d455c5c14c23%3A0x49c2401d906f4a6!3m1!7e115!4shttps%3A%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipP4WgWGyXr99V12ReGAzwwAqzgBRBScFMN3Q39U%3Dw120-h160-k-no!5stmuc%20-%20Google%20Search!15sCgIgAQ&imagekey=!1e10!2sAF1QipNMYCopuu9kCfQfWj2rwpHmO7iuIZg0G0Gu1EoR&hl=en&sa=X&ved=2ahUKEwjChY-hrPvxAhUXA2MBHUCyAqwQoiowEnoECEIQAw&viewerState=ga">PHOTOS</a>
+      </button>
+    </div>
+  )
+}
+
+export default Photos;
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -11,6 +11,7 @@ import Library from "./Library";
 import History from "./History";
 import Academics from "./Academics"
 import StudentCouncil from "./StudentCouncil";
+import Photos from "./Photos";
 
 function Index() {
   return (
@@ -19,12 +20,13 @@ function Index() {
             <i>Welcome to <br/> Tom Mboya Univeristy College</i>
             <p> As an Institution, we value the contribution of everyone. It is humans that makes an Institution. Therefore, whether you are a Student, Parent, Staff, The Public or an Administrator, your contribution to the well being of the Institution is highly appreciated.</p>
       </div>
-      
+
       <div className="Pages">
           <Login/>
           <Elearning/>
           <Portal/>
           <StudentCouncil/>
+          <Photos/>
           <Library/>
           <Downloads/>
           <Academics/>
