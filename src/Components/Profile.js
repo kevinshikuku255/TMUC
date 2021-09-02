ecc7b57e3ebc16104fe02251167dbc23a6b30b22
Kevin Shikuku <68957078+kevinsshikuku@users.noreply.github.com>
2021-09-02 19:52:14 +0300

    Update Profile.js

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,7 +1,8 @@
 import React  from 'react';
 import { useLocation } from 'react-router';
 
-import { Details } from "./Details"
+import { Details } from "./Details";
+import DetailInfo from "./Details.json";
 
 import AsaPic from "../Images/Asa.jpg";
 import DamaPic from "../Images/Dama.jpg";
@@ -10,6 +11,9 @@ import SecPic from "../Images/sec.jpg";
 import MercyPic from "../Images/Mercy.jpg";
 import KevinPic from "../Images/kevo.jpg";
 import Gitonga from "../Images/gitonga.png";
+import IconicPic from "../Images/iconic.jpg";
+import BabafemiPic from "../Images/Babfemi.jpg";
+import ColloPic from "../Images/collo.jpg";
 
 /** Profile component */
 function Profile() {
@@ -18,13 +22,17 @@ function Profile() {
 
 
   let markUp;
-  profileName === "Dama" && (markUp =  <Details name={profileName} avartar={DamaPic}/>)
-  profileName === "Asa" && (markUp =  <Details name={profileName} avartar={AsaPic}/>)
-  profileName === "Deric" && (markUp =  <Details name={profileName} avartar={DericPic}/>)
-  profileName === "Mercy" && (markUp =  <Details name={profileName} avartar={MercyPic}/>)
-  profileName === "Kevin" && (markUp =  <Details name={profileName} avartar={KevinPic}/>)
-  profileName === "Clinton" && (markUp =  <Details name={profileName} avartar={SecPic}/>)
-  profileName === "2016" && (markUp =  <Details name={""} avartar={Gitonga}/>)
+  profileName === "Dama" && (markUp =  <Details   DetailInfo={DetailInfo.Dama} avartar={DamaPic}/>)
+  profileName === "Asa" && (markUp =   <Details   DetailInfo={DetailInfo.Asa} avartar={AsaPic}/>)
+  profileName === "Deric" && (markUp =  <Details  DetailInfo={DetailInfo.Deric} avartar={DericPic}/>)
+  profileName === "Mercy" && (markUp =  <Details  DetailInfo={DetailInfo.Mercy} avartar={MercyPic}/>)
+  profileName === "Kevin" && (markUp =  <Details  DetailInfo={DetailInfo.Kevin} avartar={KevinPic}/>)
+  profileName === "Clinton" && (markUp = <Details DetailInfo={DetailInfo.Clinton} avartar={SecPic}/>)
+  profileName === "Iconic" && (markUp = <Details DetailInfo={DetailInfo.Iconic} avartar={IconicPic}/>)
+  profileName === "Babafemi" && (markUp = <Details DetailInfo={DetailInfo.Babafemi} avartar={BabafemiPic}/>)
+  profileName === "Collo" && (markUp = <Details DetailInfo={DetailInfo.Collo} avartar={ColloPic}/>)
+  profileName === "Portus" && (markUp = <Details DetailInfo={DetailInfo.Kiongozi} avartar={"Portus"}/>)
+  profileName === "2016" && (markUp =  <Details  avartar={Gitonga}/>)
 
 
   return (markUp)
