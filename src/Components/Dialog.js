f881185f9ff432765c7c8af74e4bc73237b913b3
kevin <kevinshikuku255@gmail.com>
2021-09-05 20:48:35 +0300

    dialog

--- src/Components/Dialog.js
+++ src/Components/Dialog.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import {Button, Avatar} from '@material-ui/core';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
+import {DialogActions, DialogContent} from '@material-ui/core';
 import ReactGA from 'react-ga';
 import { useLoadDispatch } from '../Context/loading';
 import Helb from '../Images/helb.jpeg';
@@ -43,18 +40,17 @@ export default function AlertDialog() {
 
   return (
     <div>
-        <DialogTitle id="alert-dialog-title">{"PROCEED TO HELB PORTAL"}</DialogTitle>
         <DialogContent>
-          <DialogContentText id="alert-dialog-description" className="dialogcontenttext">
+          <div className="dialogcontenttext">
             <Avatar src={Helb}/>
-            Higher Education Loans Board
-          </DialogContentText>
+            <b>Higher Education Loans Board</b>
+          </div>
         </DialogContent>
         <DialogActions>
           <Button onClick={deny} color="secondary">
             Back
           </Button>
-          <Button onClick={deny} color="primary">
+          <Button  color="primary">
                <a href="https://www.helb.co.ke/student-portal/" onClick={agree}> Continue </a>
           </Button>
 
