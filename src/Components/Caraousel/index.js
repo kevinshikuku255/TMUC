d6752fddd1a5ec7c966de1a782b753b46bc44b39
kevin <kevinshikuku255@gmail.com>
2021-11-20 17:29:20 +0300

    updates

--- src/Components/Caraousel/index.js
+++ src/Components/Caraousel/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import "./styles.scss";
 import Carousel from 'react-elastic-carousel';
-import { Photos } from "./Photos";
+// import { Photos } from "./Photos";
 
 
 /** Carousel component */
 function CarouselComponent() {
   return (
      <>
-     <sup>Ad</sup>
+     {/* <sup>Ad</sup> */}
       <Carousel
           itemsToShow={1} className="Carousel"
           showArrows={false}
@@ -16,11 +16,10 @@ function CarouselComponent() {
           enableAutoPlay
           pagination={false}
           >
-          <Photos/>
+          {/* <Photos/> */}
           <div className="four">
-            <h5>ROYAL DENTAL CLINIC HOMABAY</h5>
-            <p>Salama Area next to Oasis Plaza</p>
-            <i>call: 0703178781 or <a href="https://royaldental.ke/">https://royaldental.ke</a></i>
+            <h2>TOM MBOYA UNIVERSITY</h2>
+            <p>KNWOLEDGE FOR SUTAINANLE INNOVATION ENTERPRICE</p>
           </div>
         </Carousel>
         </>
