9063a4b210da285f246bcb98d254c2ee5e6add94
kevinshikuku <kevinsshikuku>
2022-04-06 01:11:20 +0300

    ...

--- src/Pages/Timetable/Event.js
+++ src/Pages/Timetable/Event.js
@@ -32,10 +32,10 @@ function Event() {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: "70%",
+    width: "75%",
     bgcolor: theme.farground,
     color: theme.primary,
-    border: '2px solid #000',
+    border: '1px solid red',
     boxShadow: 24,
     p: 4,
   };
